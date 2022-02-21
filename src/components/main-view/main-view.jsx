ca1e5ea132781c78419d537fd0c636ac02cd78d8
Sam Garcia <samuel.gsuero@gmail.com>
2022-02-21 10:16:06 +0100

    main view updated with movie card and movie view

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,17 +1,47 @@
-
 import React from 'react';
+import MovieCard from '../movie-card/movie-card';
+import MovieView from '../movie-view/movie-view';
 
 class MainView extends React.Component {
 
-  render() {
-    return (
-      <div className= 'main-view'>
-        <div>Inception</div>
-        <div>The Shawshank Redemption</div>
-        <div>Gladiator</div>
-      </div>
-    );
+  constructor() {
+    super();
+    this.state = {
+      movies: [
+        { _id: 1, Title: 'Inception', Description: 'desc1...', ImagePath: '...'},
+        { _id: 2, Title: 'The Shawshank Redemption', Description: 'desc2...', ImagePath: '...'},
+        { _id: 3, Title: 'Gladiator', Description: 'desc3...', ImagePath: '...'},
+      ],
+      selectedMovie:null
+    };
   }
+
+  setSelectedMovie(newSelectedMovie) {
+    this.setState ({
+      selectedMovie: newSelectedMovie
+    });
+  }
+
+
+//Below code renders the movies list. If its empty, displays a message
+render() {
+  const { movies, selectedMovie } = this.state;
+
+
+  if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
+
+  return (
+    <div className="main-view">
+      {selectedMovie
+        ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
+        : movies.map(movie => (
+          <MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }}/>
+        ))
+      }
+    </div>
+  );
+}
+
 }
 
 export default MainView;
