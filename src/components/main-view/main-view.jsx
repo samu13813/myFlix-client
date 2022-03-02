b772317edb00919c403dac2247485c18bd2093a8
Sam Garcia <samuel.gsuero@gmail.com>
2022-03-02 10:09:04 +0100

    login view imported into main view

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 
+import { LoginView } from '../login-view/login-view';
 import MovieCard from '../movie-card/movie-card';
 import MovieView from '../movie-view/movie-view';
 
@@ -10,7 +11,8 @@ class MainView extends React.Component {
     super();
     this.state = {
       movies: [],
-      selectedMovie:null
+      selectedMovie: null,
+      user: null,
     };
   }
 
@@ -26,9 +28,17 @@ class MainView extends React.Component {
       });
   }
 
+// When a movie is clicked, this function is called and updates the state of 'selectedMovie'
   setSelectedMovie(newSelectedMovie) {
     this.setState ({
       selectedMovie: newSelectedMovie
+    });
+  }
+
+  // When a user succesfully logs in, this function updates the 'user' property in state to that particular user
+  onLoggedIn(user) {
+    this.setState({
+      user
     });
   }
 
@@ -37,11 +47,15 @@ class MainView extends React.Component {
 render() {
   const { movies, selectedMovie } = this.state;
 
+  // If ther eis no user, the LoginView is rendered. If there is a user logged in, the user details are passed as a prop to the LoginView
+  if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
 
-  if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
+  // Before the movies have been loaded
+  if (movies.length === 0) return <div className="main-view" />;
 
   return (
     <div className="main-view">
+    // If the state of 'selectedMovie' is not null, that selected movie will be returned. Otherwhise, all movies will be returned
       {selectedMovie
         ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
         : movies.map(movie => (
