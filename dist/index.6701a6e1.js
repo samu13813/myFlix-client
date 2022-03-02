bc466ff299e5a69448481fea9212ba05d6d88e67
Sam Garcia <samuel.gsuero@gmail.com>
2022-03-02 10:54:28 +0100

    dist updated to show changes

--- dist/index.6701a6e1.js
+++ dist/index.6701a6e1.js
@@ -23027,7 +23027,9 @@ class MovieCard extends _reactDefault.default.Component {
 }
 MovieCard.propTypes = {
     movie: _propTypesDefault.default.shape({
-        Title: _propTypesDefault.default.string
+        Title: _propTypesDefault.default.string.isRequired,
+        Description: _propTypesDefault.default.string.isRequired,
+        ImagePath: _propTypesDefault.default.string.isRequired
     }).isRequired,
     onMovieClick: _propTypesDefault.default.func.isRequired
 };
